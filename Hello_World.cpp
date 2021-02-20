578f7f0879164ccd122524a788073b80be63a852
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-20 23:22:06 +0100

    Hello World
    The first thing any programmer does.

---
+++ Hello_World.cpp
@@ -0,0 +1,9 @@
+#include <iostream>                         // Include Input Ouput stream
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;         // This Line prints out "Hello World!"
+    return 0;
+}
