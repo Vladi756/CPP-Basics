b61ea781ecd87986c37497b6b4e94cd1904739b2
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-21 21:05:27 +0100

    Variables
    Simple program which shows how variables work in C++

--- Variables.cpp
+++ Variables.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main()
 {
     string characterName = "Adam";
-    int characterAge = 40;              // Declaring and initalizing variables
+    int characterAge = 40;                                                  // Declaring and initalizing variables
 
     cout << "There once was a man named " << characterName << "."<< endl;
     cout << "He was  years old " << characterAge << "."<< endl;
     cout << "He liked the name " << characterName << "." << endl;
-    cout << "But did not like being" << characterAge << "." << endl;    // Using the variables.
+    cout << "But did not like being" << characterAge << "." << endl;        // Using the variables.
 
     return 0;
 }
