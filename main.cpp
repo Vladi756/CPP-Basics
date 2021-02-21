21e3c8f18f13b4216d2f0f9402e0ea11b24e8e0b
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-21 21:22:16 +0100

    Data Type

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grade = 'A';                   // A character
+    string phrase = "Hello";            // A string is a chracter array - so like a word
+    int number = 5;                     // Whole numbers
+    float average = 5.4;                // Floating point number
+    double specificNumber = 5.45;       // Doubles can store more decimals than floats
+    boolean isMale = true;              // True/False
+
+    return 0;
+}
