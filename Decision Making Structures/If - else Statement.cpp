7b0a513ee55603df65711d7ce513c28b3a8273f6
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-22 23:41:28 +0100

    If/Else Statement
    One of the most basic decision making structures in C++, the if statement. Technically, it's an if/else-if statement, but they work very similarly so this should make sense.

--- Decision Making Structures/If - else Statement.cpp
+++ Decision Making Structures/If - else Statement.cpp
@@ -7,15 +7,15 @@ using namespace std;
 int main()
 {
     bool isMale = true;
-    bool isTall = false;                    // Boolean variables upon which the output of the program depends on.
+    bool isTall = false;                               // Boolean variables upon which the output of the program depends on.
 
     if(isMale && isTall) {                             // If statement checks condition - the truth of the isMale variable
         cout << "You are a tall male." << endl;        // When isMale and isTall are both T
-    }else if(isMale) {
+    } else if(isMale) {
         cout << "You are a male." << endl;             // isMale T; isTall F;
-    }else if(isTall) {
+    } else if(isTall) {
         cout << "You are a tall female." << endl;      // isMalt F; isTall T;
-    }else {
+    } else {
         cout << "You are a female." << endl;           // isMale F; isTall F;
     }
 }
