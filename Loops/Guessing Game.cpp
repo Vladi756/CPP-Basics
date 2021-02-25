5c0b85240e01d3c315ab4503f8c513d73a074327
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-25 16:28:53 +0100

    Guessing Game
    Simple number guessing game in C++ using a do while loop.

--- Loops/Guessing Game.cpp
+++ Loops/Guessing Game.cpp
@@ -5,18 +5,18 @@ using namespace std;
 int main()
 {
     int secretNum  = 7;
-    int guess, i = 0; // i = guess count
+    int guess, i = 0; //    i = guess count
 
     do {
         cout << "Guess a number I'm thinking of (1 to 10): " << endl;
         cin >> guess;
         i++;
-    }while(guess != secretNum && i < 3);
+    } while (guess != secretNum && i < 3);        // 3 guesses 
 
     if(guess == secretNum) {
-       cout << "Good Job!";
+       cout << "Good Job!";     // If user guesses correctly
     } else {
-        cout << "No more guesses for you!";
+        cout << "No more guesses for you!";     // If user loses the game.
     }
     return 0;
 }
