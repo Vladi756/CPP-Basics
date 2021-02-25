bf1ae096c2984cc84e3504a855587045b46e2383
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-25 14:55:40 +0100

    While Loop
    asdf

---
+++ Loops/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int index = 1;
+
+    while(index <= 5) {             // Condition of while loop.
+        cout << "Hello!" << endl;
+        index++;                    // Body of while loop.
+    }
+
+    return 0;
+}
